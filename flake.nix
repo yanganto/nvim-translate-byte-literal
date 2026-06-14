a887067ad8fb38f39e1c07919b0124985d0b8d4f
Antonio Yang <yanganto@gmail.com>
2026-06-14 15:56:54 +0800

    update rust 2024 edition & nixos 26.05

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05-small";
 
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-extern crate neovim_lib;
-
 #[cfg(test)]
 mod tests;
 
